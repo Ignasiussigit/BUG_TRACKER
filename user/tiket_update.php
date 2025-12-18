1249f8834b48e38f42c78bc8c6c247e4ce5e554c
Ignasiussigit <sigitignasius@gmail.com>
2025-12-18 22:12:04 +0700

    fix bug update muncul tag html di summernote

--- user/tiket_update.php
+++ user/tiket_update.php
@@ -2,35 +2,55 @@
 session_start();
 date_default_timezone_set('Asia/Jakarta');
 include '../koneksi.php';
-$tanggal = date('Y-m-d');
 
-$id  = mysqli_real_escape_string($koneksi, htmlspecialchars($_POST['id']));
-$urgency  = mysqli_real_escape_string($koneksi, htmlspecialchars($_POST['urgency']));
-$email  = mysqli_real_escape_string($koneksi, htmlspecialchars($_POST['email']));
-$departemen  = mysqli_real_escape_string($koneksi, htmlspecialchars($_POST['departemen']));
-$judul  = mysqli_real_escape_string($koneksi, htmlspecialchars($_POST['judul']));
-$keterangan  = mysqli_real_escape_string($koneksi, htmlspecialchars($_POST['keterangan']));
+$id = mysqli_real_escape_string($koneksi, $_POST['id']);
 
+$urgency    = mysqli_real_escape_string($koneksi, $_POST['urgency']);
+$email      = mysqli_real_escape_string($koneksi, $_POST['email']);
+$departemen = mysqli_real_escape_string($koneksi, $_POST['departemen']);
+$judul      = mysqli_real_escape_string($koneksi, $_POST['judul']);
+$keterangan = mysqli_real_escape_string($koneksi, $_POST['keterangan']);
 
 // cek gambar
-$rand = rand();
-$allowed =  array('gif','png','jpg','jpeg','PNG','JPG','JPEG');
+$rand     = rand();
+$allowed  = ['gif','png','jpg','jpeg','PNG','JPG','JPEG'];
 $filename = $_FILES['gambar']['name'];
-$ext = pathinfo($filename, PATHINFO_EXTENSION);
 
-if($filename==""){	
-	mysqli_query($koneksi,"update pengaduan set pengaduan_judul='$judul', pengaduan_keterangan='$keterangan', pengaduan_urgency='$urgency', pengaduan_email='$email', pengaduan_departemen='$departemen' where pengaduan_id='$id'");
-	header("location:tiket.php?alert=edit");
-}else{
-	if(!in_array($ext,$allowed) ) {
-		header("location:pengaduan.php?alert=gagal");
-	}else{
-		move_uploaded_file($_FILES['gambar']['tmp_name'], '../gambar/tiket/'.$rand.'_'.$filename);
-		$x = $rand.'_'.$filename;
-		mysqli_query($koneksi,"update pengaduan set pengaduan_judul='$judul', pengaduan_keterangan='$keterangan', pengaduan_urgency='$urgency', pengaduan_email='$email', pengaduan_departemen='$departemen', pengaduan_gambar='$x' where pengaduan_id='$id'");
-	header("location:tiket.php?alert=edit");
-	}
+if ($filename == "") {
+
+    mysqli_query($koneksi,"
+        UPDATE pengaduan SET
+            pengaduan_judul = '$judul',
+            pengaduan_keterangan = '$keterangan',
+            pengaduan_urgency = '$urgency',
+            pengaduan_email = '$email',
+            pengaduan_departemen = '$departemen'
+        WHERE pengaduan_id = '$id'
+    ");
+
+} else {
+
+    $ext = pathinfo($filename, PATHINFO_EXTENSION);
+
+    if (!in_array($ext, $allowed)) {
+        header("location:tiket.php?alert=gagal");
+        exit;
+    }
+
+    $file = $rand . '_' . $filename;
+    move_uploaded_file($_FILES['gambar']['tmp_name'], '../gambar/tiket/' . $file);
+
+    mysqli_query($koneksi,"
+        UPDATE pengaduan SET
+            pengaduan_judul = '$judul',
+            pengaduan_keterangan = '$keterangan',
+            pengaduan_urgency = '$urgency',
+            pengaduan_email = '$email',
+            pengaduan_departemen = '$departemen',
+            pengaduan_gambar = '$file'
+        WHERE pengaduan_id = '$id'
+    ");
 }
 
-
-
+header("location:tiket.php?alert=edit");
+exit;
