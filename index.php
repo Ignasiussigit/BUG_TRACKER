bc22cc9ef4778fee60ef3f0b65503d4318ce1409
Ignasiussigit <martixdna@gmail.com>
2025-12-23 09:40:51 +0700

    mengubah text di index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <section class="bg-gradient-2 py-3 hero">
     <div class="container">
         <div class="py-5 text-center hero-text">
-            <h2 class="text-light mb-2">Selamat Datang di Layanan BUG TRACKER <br>Rs.St.Elisabeth Bekasi</h2>
+            <h2 class="text-light mb-2">Selamat Datang di Layanan IT HELPDESK <br>Rs.St.Elisabeth Bekasi</h2>
             <div class="container">
                 <div class="row justify-content-center text-center">
                   <a class="navbar-brand mb-3" href="index.php"><img style="height:130px; width:130px; margin-top:20px;" src="gambar/sistem/logo.png" alt=""></a> <br>                  
@@ -21,7 +21,6 @@
             <div class="mt-5">
                 <a href="login_user.php" class="btn mx-1 px-4 py-2 btn-danger">LOGIN &nbsp;</a> 
                 <!-- <a href="daftar_user.php" class="btn mx-1 px-4 py-2 btn-danger">DAFTAR &nbsp;</a>  -->
-
             </div>
         </div>
     </div>
